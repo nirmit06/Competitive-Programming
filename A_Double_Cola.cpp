4fa47336cb47cb67e10fd48c174a78e111c0775b
nirmit06 <nirmit53.nsp@gmail.com>
2022-02-12 19:53:48 +0530

    Added CP code

---
+++ A_Double_Cola.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+double kthRoot(double n, int k)
+{
+    return pow(k, (1.0 / k) * (log(n) / log(k)));
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int r = 1;
+    while (r * 5 < n)
+    {
+        n -= r * 5;
+        r *= 2;
+    }
+
+    string names[] = {"Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
+    cout << names[(n - 1) / r] << endl;
+
+
+    return 0;
+}
